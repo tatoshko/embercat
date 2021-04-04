f16ff410e0384a1ebd4397598aa7f3fa1a679b28
tatoshko <tatoshko@gmail.com>
2021-04-04 12:46:30 +0300

    auto

--- core/2ch.go
+++ core/2ch.go
@@ -6,11 +6,15 @@ import (
     "io"
     "log"
     "net/http"
+    "regexp"
 )
 
 var CATALOG = "https://2ch.hk/b/catalog.json"
 
-func Init() {
+func parseThreads() []Thread {
+    var threads []Thread = make([]Thread, 10)
+    var matcher = regexp.MustCompile(`(webm|вебм|цуин|mp4)`)
+
     if res, err := http.Get(CATALOG); err != nil {
         log.Fatalln(err)
     } else {
@@ -24,13 +28,20 @@ func Init() {
             if err = json.Unmarshal(body, &board); err != nil {
                 log.Fatalln(err)
             } else {
-                log.Printf("Subject of first thread %q", board.Threads[0].Subject)
+                for _, thread := range board.Threads {
+                    if matcher.MatchString(thread.Subject) {
+                        threads = append(threads, thread)
+                    }
+                }
             }
         }
-
     }
+
+    return threads
 }
 
 func handle2ch(bot *tgbotapi.BotAPI, update tgbotapi.Update, data string) {
-    Init()
+    for _, thread := range parseThreads() {
+        log.Printf("Thread: %s", thread.Subject)
+    }
 }
