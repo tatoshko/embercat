4990712e92447f964244a4d974d452c4b33acda5
tatoshko <tatoshko@gmail.com>
2021-04-04 10:35:18 +0300

    auto

--- core/bot.go
+++ core/bot.go
@@ -6,31 +6,61 @@ import (
     "regexp"
 )
 
+type CommandHandler func(*tba.BotAPI, tba.Update, string)
+
+type TBot struct {
+    Bot *tba.BotAPI
+    commandMsg *regexp.Regexp
+    updates tba.UpdatesChannel
+
+    HANDLERS map[string]CommandHandler
+}
+
 var err error
 var Bot *tba.BotAPI
-var commandMsg = regexp.MustCompile(`^\/(?P<command>\w+)\s*(?P<data>.*)$`)
 
-func InitBot(token, hook string) {
+func InitBot(token, hook string) (tbot *TBot) {
+    tbot = &TBot{
+        Bot:        nil,
+        commandMsg: regexp.MustCompile(`^\/(?P<command>\w+)\s*(?P<data>.*)$`),
+        HANDLERS:   make(map[string]CommandHandler),
+    }
+
+    tbot.RegisterHandler("set", handleSet)
+    tbot.RegisterHandler("get", handleGet)
+
+
     if Bot, err = tba.NewBotAPI(token); err != nil {
         panic(err)
     }
 
-    Bot.SetWebhook(tba.NewWebhook(hook + "/" + token))
-    updates := Bot.ListenForWebhook("/" + Bot.Token)
+    tbot.Bot = Bot
 
-    for update := range updates {
+    Bot.SetWebhook(tba.NewWebhook(hook + "/" + token))
+    tbot.updates = Bot.ListenForWebhook("/" + Bot.Token)
+
+    return
+}
+
+func (bot *TBot) RegisterHandler(name string, f CommandHandler) {
+    bot.HANDLERS[name] = f
+}
+
+func (bot *TBot) UnregisterHandler(name string) {
+    delete(bot.HANDLERS, name)
+}
+
+func (bot *TBot) Watch() {
+    for update := range bot.updates {
         text := update.Message.Text
 
-        if commandMsg.MatchString(text) {
-            match := reSubMatchMap(commandMsg, text)
+        if bot.commandMsg.MatchString(text) {
+            match := reSubMatchMap(bot.commandMsg, text)
 
-            log.Printf("Command: %s, data: %s", match["command"], match["data"])
+            log.Printf("Command: '%s', data: '%s'", match["command"], match["data"])
 
-            switch match["command"] {
-            case "set":
-                handleSet(update, match["data"])
-            case "get":
-                handleGet(update, match["data"])
+            if handler, found := bot.HANDLERS[match["command"]]; found {
+                go handler(bot.Bot, update, match["data"])
             }
         }
     }
--- core/storage.go
+++ core/storage.go
@@ -2,38 +2,38 @@ package core
 
 import (
     "fmt"
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    tba "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
     "strings"
 )
 
 var Storage = make(map[string]string)
 
-func handleSet(update tgbotapi.Update, text string) {
+func handleSet(bot *tba.BotAPI, update tba.Update, text string) {
     parts := strings.SplitAfterN(text, " ", 2)
     key, value := parts[0], parts[1]
     Storage[key] = value
 
-    msg := tgbotapi.NewMessage(
+    msg := tba.NewMessage(
         update.Message.Chat.ID,
         fmt.Sprintf("'%s' has been set to key '%s'", value, key),
     )
     msg.ReplyToMessageID = update.Message.MessageID
 
-    Bot.Send(msg)
+    bot.Send(msg)
 }
 
-func handleGet(update tgbotapi.Update, key string) {
+func handleGet(bot *tba.BotAPI, update tba.Update, key string) {
     log.Printf("Getted from storage %q, by key %s, result %s", Storage, key, Storage[key])
 
     value, found := Storage[key]
 
-    msg := tgbotapi.NewMessage(
+    msg := tba.NewMessage(
         update.Message.Chat.ID,
         fmt.Sprintf("Value is %v *%s*", found, value),
     )
 
     msg.ReplyToMessageID = update.Message.MessageID
 
-    Bot.Send(msg)
+    bot.Send(msg)
 }
