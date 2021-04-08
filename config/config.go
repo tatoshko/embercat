94fb8d14506861795d2629e5230f75746ff7f84a
tatoshko <tatoshko@gmail.com>
2021-04-08 20:21:08 +0300

    auto

--- config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type Config struct {
-    Token string `json:"token"`
-    Hook  string `json:"hook"`
-    DB    string `json:"db"`
-}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
     "encoding/json"
     "github.com/tatoshko/tbot/assets"
-    config2 "github.com/tatoshko/tbot/config"
     "github.com/tatoshko/tbot/core"
     "io/ioutil"
     "net/http"
@@ -13,13 +12,19 @@ import (
 var err error
 var PORT = os.Getenv("PORT")
 
+type Config struct {
+    Token string `json:"token"`
+    Hook  string `json:"hook"`
+    DB    string `json:"db"`
+}
+
 func main() {
     var jsonFile *os.File
 
     if jsonFile, err = os.Open("config.json"); err == nil {
         defer jsonFile.Close()
 
-        var config config2.Config
+        var config Config
         bytes, _ := ioutil.ReadAll(jsonFile)
 
         if err = json.Unmarshal(bytes, &config); err != nil {
