ef0af1fc38b6ea7bc1028c331ecff8410a2761fc
Anton Kim <tatoshko@gmail.com>
2023-10-30 21:23:00 +0300

    f

--- redis/redis.go
+++ redis/redis.go
@@ -1,37 +1,12 @@
 package redis
 
 import (
-	"gopkg.in/redis.v3"
-	"log"
-	"net/url"
-	"os"
-	"strings"
+    "gopkg.in/redis.v3"
 )
 
-var REDIS_URL = os.Getenv("REDIS_URL")
-
 func GetClient() *redis.Client {
-	if REDIS_URL == "" {
-		log.Printf("Redis can't be inited, because of empty HerokuURL")
-		return nil
-	}
-
-	log.Printf("HerokuURL %s", REDIS_URL)
-
-	var password string
-	var addr string
-
-	if !strings.Contains(REDIS_URL, "localhost") {
-		parsedURL, _ := url.Parse(REDIS_URL)
-		password, _ = parsedURL.User.Password()
-		addr = parsedURL.Host
-	}
-
-	log.Printf("Connecting to ADDR: %s, PASWORD: %s", addr, password)
-
-	return redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: password,
-		DB:       0,
-	})
+    return redis.NewClient(&redis.Options{
+        Addr: "127.0.0.1",
+        DB:   1,
+    })
 }
