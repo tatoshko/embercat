26692d42e4a91c3b6215c673aec1f8803e642b7e
Anton Kim <tatoshko@gmail.com>
2023-10-30 21:05:01 +0300

    f

--- main.go
+++ main.go
@@ -1,48 +1,54 @@
 package main
 
 import (
-	"embercat/assets"
-	"embercat/bot"
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+    "embercat/assets"
+    "embercat/bot"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
 )
 
 var err error
-var PORT = os.Getenv("PORT")
+var HOST = ""
+var PORT = ":3001"
 
 type Config struct {
-	Name  string `json:"name"`
-	Token string `json:"token"`
-	Hook  string `json:"hook"`
+    Name  string `json:"name"`
+    Token string `json:"token"`
+    Hook  string `json:"hook"`
 }
 
 func main() {
-	var jsonFile *os.File
+    var jsonFile *os.File
 
-	if jsonFile, err = os.Open("config.json"); err == nil {
-		defer jsonFile.Close()
+    if jsonFile, err = os.Open("config.json"); err == nil {
+        defer jsonFile.Close()
 
-		var config Config
-		bytes, _ := ioutil.ReadAll(jsonFile)
+        var config Config
+        bytes, _ := ioutil.ReadAll(jsonFile)
 
-		if err = json.Unmarshal(bytes, &config); err != nil {
-			panic(err)
-		}
+        if err = json.Unmarshal(bytes, &config); err != nil {
+            panic(err)
+        }
 
-		assets.InitBox()
+        assets.InitBox()
 
-		go (func() {
-			if err := http.ListenAndServe("0.0.0.0:"+PORT, nil); err != nil {
-				log.Fatalln(err.Error())
-			}
-		})()
+        go (func() {
+            addr := fmt.Sprintf("%s%s", HOST, PORT)
 
-		bot.Start(config.Name, config.Token, config.Hook)
-	} else {
-		panic(err)
-	}
+            log.Printf("Bind to: [%s]", addr)
+
+            if err := http.ListenAndServe(addr, nil); err != nil {
+                log.Fatalln(err.Error())
+            }
+        })()
+
+        bot.Start(config.Name, config.Token, config.Hook)
+    } else {
+        panic(err)
+    }
 
 }
