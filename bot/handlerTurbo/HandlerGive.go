0b905d012a9dbd9e68b61ebf9a8a0aad38477bcc
Anton Kim <tatoshko@gmail.com>
2023-11-02 21:04:31 +0300

    auto

--- bot/handlerTurbo/HandlerGive.go
+++ bot/handlerTurbo/HandlerGive.go
@@ -1,6 +1,7 @@
 package handlerTurbo
 
 import (
+    redis2 "embercat/redis"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
     "regexp"
@@ -8,7 +9,16 @@ import (
 )
 
 func HandlerGive(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    redis := redis2.GetClient()
+    if redis == nil {
+        return
+    }
+    defer redis.Close()
+
     chantID := update.Message.Chat.ID
+    userID := update.Message.From.ID
+
+    localCollectionKey := makeLocalKey(REDIS_KEY_TURBO_COLLECTION, userID)
 
     validCommand := regexp.MustCompile(`^/give\s\d{3}\s\@\w+$`)
     text := update.Message.Text
@@ -19,7 +29,13 @@ func HandlerGive(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
         liner := parts[1]
         recipient := parts[2]
 
-        log.Printf("%s -> %s", liner, recipient)
+        log.Printf("Trying to give %s to %s", liner, recipient)
+
+        if stats, err := redis.ZRangeWithScores(localCollectionKey, 0, -1).Result(); err != nil {
+            log.Printf("HandlerCollection ZRangeWithScores error %s", err.Error())
+        } else {
+            log.Printf("%v", stats)
+        }
     } else {
         log.Printf("Incorrect command '%s'", text)
         msg := tgbotapi.NewMessage(chantID, "Неверный формат команды. Пример:\n<code>/give 001 @username</code>")
