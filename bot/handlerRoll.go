feb81fa073743568cdd7218207e4f0f3cb704eaf
Anton Kim <tatoshko@gmail.com>
2023-11-12 00:32:21 +0300

    auto

--- bot/handlerRoll.go
+++ bot/handlerRoll.go
@@ -1,17 +1,18 @@
 package bot
 
 import (
-	"fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"log"
-	"time"
+    "fmt"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    "log"
+    "time"
 )
 
 func handleRoll(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("https://animego.org/anime/random?%d", time.Now().Unix()))
-	msg.ReplyToMessageID = update.Message.MessageID
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("https://animego.org/anime/random?%d", time.Now().Unix()))
+    msg.ReplyToMessageID = update.Message.MessageID
+    msg.DisableWebPagePreview = false
 
-	if _, err := bot.Send(msg); err != nil {
-		log.Printf("handleRoll error %s\n", err.Error())
-	}
+    if _, err := bot.Send(msg); err != nil {
+        log.Printf("handleRoll error %s\n", err.Error())
+    }
 }
