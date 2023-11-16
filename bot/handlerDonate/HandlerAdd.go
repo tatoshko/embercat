f88f1b734ae89fbf905ccd74994754ffc3a7d292
Andrei Godlevskii <godlevskii.andrei@gmail.com>
2023-11-16 16:45:49 +0100

    Donates: Handle incorrect user
    * Send the respectful notification to the non-admin user who try to add anyone to the list of supporters.

--- bot/handlerDonate/HandlerAdd.go
+++ bot/handlerDonate/HandlerAdd.go
@@ -3,33 +3,43 @@ package handlerDonate
 import (
 	"embercat/bot/types"
 	redis2 "embercat/redis"
+	"errors"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func HandlerAdd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 	userID := update.Message.From.UserName
+
 	if userID != "tatoshko" {
-		log.Print("HandlerAdd error this user cannot donate\n")
-		return
+		msg.Text = getUserDonateMessage()
+	} else {
+		if info, err := addDonater(update.Message.CommandArguments()); err != nil {
+			log.Printf("HandlerAdd error %s\n", err.Error())
+			return
+		} else {
+			msg.Text = getDonateMessage(info)
+		}
 	}
 
+	if _, err := bot.Send(msg); err != nil {
+		log.Printf("HandlerAdd error %s\n", err.Error())
+	}
+}
+
+func addDonater(args string) (redis2.Z, error) {
 	redis := redis2.GetClient()
 	if redis == nil {
-		return
+		return redis2.Z{}, errors.New("failed to get the redis client")
 	}
 	defer redis.Close()
 
-	info, err := newDonateInfo(update.Message.CommandArguments())
-	if err != nil {
-		log.Printf("HandlerAdd error %s\n", err.Error())
-		return
-	}
-
-	redis.ZAdd(types.REDIS_SUPPORTERS_COLLECTION, info)
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, getDonateMessage(info))
-	if _, err := bot.Send(msg); err != nil {
-		log.Printf("HandlerAdd error %s\n", err.Error())
+	if info, err := newDonateInfo(args); err != nil {
+		return info, err
+	} else {
+		redis.ZAdd(types.REDIS_SUPPORTERS_COLLECTION, info)
+		return info, nil
 	}
 }
