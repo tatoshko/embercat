2af55e6ef0e5a6975101acaf1e60026b4a1ceec8
Anton Kim <tatoshko@gmail.com>
2023-11-19 19:39:27 +0300

    auto

--- bot/handlerPic/core.go
+++ bot/handlerPic/core.go
@@ -2,6 +2,12 @@ package handlerPic
 
 import "log"
 
+const (
+    CDN       = "https://pics.useful.team"
+    REDIS_KEY = "pic:anime"
+    MAX_PICS  = 409
+)
+
 func getLogger(scope string) func(s string) {
     return func(s string) {
         log.Printf("Handler Pic: [%s] %s", scope, s)
