318c370865ce1818e806635e24abfdb193b816e0
Anton Kim <tatoshko@gmail.com>
2023-11-19 15:58:37 +0300

    auto

---
+++ bot/handlerPic/logger.go
@@ -0,0 +1,7 @@
+package handlerPic
+
+import "log"
+
+func logger(scoope, s string) {
+    log.Printf("Handler Pic: [%s] %s", scoope, s)
+}
--- bot/handlerPic/rnd.go
+++ bot/handlerPic/rnd.go
@@ -1,27 +1,31 @@
 package handlerPic
 
 import (
-    "fmt"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
-    "math/rand"
-    "strings"
-    "time"
 )
 
-const MAX = 409
-const CDN = "https://pics.useful.team"
-
 func Rnd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    var err error
     chatID := update.Message.Chat.ID
 
-    rand.Seed(time.Now().UnixMicro())
-    i := rand.Intn(MAX)
-    id := fmt.Sprintf("%05d", i)
-    link := strings.Join([]string{CDN, id}, "/")
+    pic := NewPicRND()
 
-    msg := tgbotapi.NewMessage(chatID, link)
-    msg.DisableWebPagePreview = false
+    //resp, err := http.Get(pic.URL())
+    //if err != nil {
+    //    logger("HTTP.GET", err.Error())
+    //    return
+    //}
+    //defer resp.Body.Close()
+    //
+    //if resp.StatusCode != 200 {
+    //    logger("STATUS", string(resp.StatusCode))
+    //    return
+    //}
+    //
+    //img, _, err := image.Decode(resp.Body)
+
+    msg := tgbotapi.NewPhotoUpload(chatID, tgbotapi.File{FilePath: pic.URL()})
 
     if _, err := bot.Send(msg); err != nil {
         log.Printf("Pic.RND send message error: %s", err.Error())
