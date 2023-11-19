535c93c44b6cef91a6931ec170892b0f1f963933
Anton Kim <tatoshko@gmail.com>
2023-11-19 16:01:40 +0300

    auto

--- bot/handlerPic/rnd.go
+++ bot/handlerPic/rnd.go
@@ -1,8 +1,10 @@
 package handlerPic
 
 import (
+    "fmt"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
+    "time"
 )
 
 func Rnd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
@@ -26,6 +28,7 @@ func Rnd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
     //img, _, err := image.Decode(resp.Body)
 
     msg := tgbotapi.NewPhotoUpload(chatID, tgbotapi.File{FilePath: pic.URL()})
+    msg.Caption = fmt.Sprintf("%v", time.Now())
 
     if _, err := bot.Send(msg); err != nil {
         log.Printf("Pic.RND send message error: %s", err.Error())
