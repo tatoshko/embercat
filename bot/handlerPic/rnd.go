e15b2d9b89abf1d027525b5c605d901b2fdbca8f
Anton Kim <tatoshko@gmail.com>
2023-11-19 16:08:30 +0300

    auto

--- bot/handlerPic/rnd.go
+++ bot/handlerPic/rnd.go
@@ -3,31 +3,37 @@ package handlerPic
 import (
     "fmt"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    "io"
     "log"
+    "net/http"
     "time"
 )
 
 func Rnd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-    //var err error
+    var err error
     chatID := update.Message.Chat.ID
 
     pic := NewPicRND()
 
-    //resp, err := http.Get(pic.URL())
-    //if err != nil {
-    //    logger("HTTP.GET", err.Error())
-    //    return
-    //}
-    //defer resp.Body.Close()
-    //
-    //if resp.StatusCode != 200 {
-    //    logger("STATUS", string(resp.StatusCode))
-    //    return
-    //}
-    //
-    //img, _, err := image.Decode(resp.Body)
+    resp, err := http.Get(pic.URL())
+    if err != nil {
+        logger("HTTP.GET", err.Error())
+        return
+    }
+    defer resp.Body.Close()
 
-    msg := tgbotapi.NewPhotoUpload(chatID, tgbotapi.File{FilePath: pic.URL()})
+    if resp.StatusCode != 200 {
+        logger("STATUS", string(resp.StatusCode))
+        return
+    }
+
+    var b []byte
+    if b, err = io.ReadAll(resp.Body); err != nil {
+        logger("READ PIC", err.Error())
+        return
+    }
+
+    msg := tgbotapi.NewPhotoUpload(chatID, tgbotapi.FileBytes{Name: pic.URL(), Bytes: b})
     msg.Caption = fmt.Sprintf("%v", time.Now())
 
     if _, err := bot.Send(msg); err != nil {
