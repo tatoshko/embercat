33615a91d64438fea0277972276811e451ed0975
Anton Kim <tatoshko@gmail.com>
2023-11-19 16:50:25 +0300

    auto

--- bot/handlerPic/save.go
+++ bot/handlerPic/save.go
@@ -1,14 +1,15 @@
 package handlerPic
 
 import (
-    "fmt"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func Save(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
     reply := update.Message.ReplyToMessage
 
-    if reply != nil {
-        logger("PSAVE", fmt.Sprintf("%v", reply.Photo))
+    if reply != nil && reply.Photo != nil {
+        for _, pic := range *reply.Photo {
+            logger("PSAVE", pic.FileID)
+        }
     }
 }
