1d4f741360ebf8a77605ce71f8b3ebde8ccf4aa6
Anton Kim <tatoshko@gmail.com>
2023-11-20 18:36:51 +0300

    auto

--- bot/handlerPic/save.go
+++ bot/handlerPic/save.go
@@ -1,7 +1,7 @@
 package handlerPic
 
 import (
-    redis2 "embercat/redis"
+    "embercat/pgsql"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -26,15 +26,9 @@ func Save(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
         break
     }
 
-    var err error
-    redis := redis2.GetClient()
-    if redis == nil {
-        return
-    }
-    defer redis.Close()
-
-    if _, err = redis.SAdd(REDIS_KEY, photoID).Result(); err != nil {
+    pg := pgsql.GetClient()
+    q := `insert into anime (photoid) values ($1)`
+    if _, err := pg.Exec(q, photoID); err != nil {
         logger(err.Error())
-        return
     }
 }
