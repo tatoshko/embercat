74d4b6ae71e94c55174fec35ee770c5dbf2497c6
Anton Kim <tatoshko@gmail.com>
2023-12-13 11:38:21 +0300

    auto

--- scripts/init_tables.sql
+++ scripts/init_tables.sql
@@ -23,3 +23,17 @@ CREATE TABLE IF NOT EXISTS anime (
     id uuid default uuid_generate_v4() primary key,
     photoId varchar unique not null
 );
+
+create table if not exists words (
+    word varchar(32) not null unique,
+    len int not null,
+    count int not null default 1
+);
+create or replace function calc_word_len() returns trigger as $cwl$
+    begin
+        new.len = length(new.word);
+        return new;
+    end;
+$cwl$ LANGUAGE plpgsql;
+create or replace trigger set_word_len before insert or update on words
+    for each row execute procedure calc_word_len();
