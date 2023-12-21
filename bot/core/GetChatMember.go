d990c8c776fb702b0c01deadcd1046a9d2cb473e
Anton Kim <tatoshko@gmail.com>
2023-12-21 13:57:32 +0300

    auto

--- bot/core/GetChatMember.go
+++ bot/core/GetChatMember.go
@@ -3,20 +3,15 @@ package core
 import (
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
-    "strconv"
 )
 
-func GetChatMember(bot *tgbotapi.BotAPI, chatID int64, userID string) (member tgbotapi.ChatMember, err error) {
-    if id, err := strconv.Atoi(userID); err == nil {
-        config := tgbotapi.ChatConfigWithUser{
-            ChatID: chatID,
-            UserID: id,
-        }
-        if member, err = bot.GetChatMember(config); err != nil {
-            log.Printf("HandleReg GetChatMember err %s", err.Error())
-        }
-    } else {
-        log.Printf("USER_ID '%s', %q", err.Error(), id)
+func GetChatMember(bot *tgbotapi.BotAPI, chatID int64, userID int) (member tgbotapi.ChatMember, err error) {
+    config := tgbotapi.ChatConfigWithUser{
+        ChatID: chatID,
+        UserID: userID,
+    }
+    if member, err = bot.GetChatMember(config); err != nil {
+        log.Printf("HandleReg GetChatMember err %s", err.Error())
     }
 
     return
