9f14e32034e529efd91a9c875eea5bdb35fc103b
Anton Kim <tatoshko@gmail.com>
2023-12-21 13:22:53 +0300

    auto

---
+++ bot/handlerWednesday/all.go
@@ -0,0 +1,38 @@
+package handlerWednesday
+
+import (
+    "database/sql"
+    "embercat/pgsql"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func All(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    var err error
+    logger := getLogger("ALL")
+
+    pg := pgsql.GetClient()
+    q := `select id, frog from frog`
+
+    var rows *sql.Rows
+    if rows, err = pg.Query(q); err != nil {
+        logger(err.Error())
+        return
+    }
+    defer rows.Close()
+
+    msg := tgbotapi.NewPhotoShare(update.Message.Chat.ID, "")
+    for rows.Next() {
+        var id, frog string
+        if err = rows.Scan(&id, &frog); err != nil {
+            logger(err.Error())
+            return
+        }
+
+        msg.Caption = id
+        msg.FileID = frog
+
+        if _, err = bot.Send(msg); err != nil {
+            logger(err.Error())
+        }
+    }
+}
