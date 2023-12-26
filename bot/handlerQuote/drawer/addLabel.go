8dadb40abaf05fef5e82cbb5ed7544d1fdb82d85
Anton Kim <tatoshko@gmail.com>
2023-12-26 15:57:09 +0300

    auto

--- bot/handlerQuote/drawer/addLabel.go
+++ bot/handlerQuote/drawer/addLabel.go
@@ -1,13 +1,12 @@
 package drawer
 
 import (
+    "github.com/golang/freetype"
     "github.com/golang/freetype/truetype"
-    "golang.org/x/image/font"
     "golang.org/x/image/font/gofont/gobold"
     "golang.org/x/image/math/fixed"
     "image"
     "image/color"
-    "log"
 )
 
 var (
@@ -24,28 +23,35 @@ func AddLabel(img *image.RGBA, label string) {
     srcImg = img
     ib := img.Bounds()
     imageBounds = fixed.R(ib.Min.X, ib.Min.Y, ib.Max.X, ib.Max.Y)
-    drawString(label, StartFontSize)
+    //drawString(label, StartFontSize)
+
+    pt := freetype.Pt(0, 0)
+
+    ctx := freetype.NewContext()
+    ctx.SetDPI(72)
+    ctx.SetDst(img)
+    ctx.DrawString(label, pt)
 }
 
-func drawString(label string, size fixed.Int26_6) {
-    if size == 0 {
-        return
-    }
-
-    point = fixed.Point26_6{X: imageBounds.Max.X - 16*fixed.Int26_6(DPI/4), Y: imageBounds.Max.Y - size*fixed.Int26_6(DPI/4)}
-    drawer := &font.Drawer{Dst: srcImg, Src: image.NewUniform(white), Face: getFace(size), Dot: point}
-
-    sb, _ := drawer.BoundString(label)
-
-    if sb.In(imageBounds) {
-        log.Printf("OK SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
-        drawer.DrawString(label)
-    } else {
-        log.Printf("FAIL SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
-        drawString(label, size-1)
-    }
-}
-
-func getFace(size fixed.Int26_6) font.Face {
-    return truetype.NewFace(TTF, &truetype.Options{Size: float64(size), DPI: DPI})
-}
+//func drawString(label string, size fixed.Int26_6) {
+//    if size == 0 {
+//        return
+//    }
+//
+//    point = fixed.Point26_6{X: 16 * fixed.Int26_6(DPI/4), Y: imageBounds.Max.Y - size*fixed.Int26_6(DPI/4)}
+//    drawer := &font.Drawer{Dst: srcImg, Src: image.NewUniform(white), Face: getFace(size), Dot: point}
+//
+//    sb, _ := drawer.BoundString(label)
+//
+//    if sb.In(imageBounds) {
+//        log.Printf("OK SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
+//        drawer.DrawString(label)
+//    } else {
+//        log.Printf("FAIL SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
+//        drawString(label, size-1)
+//    }
+//}
+//
+//func getFace(size fixed.Int26_6) font.Face {
+//    return truetype.NewFace(TTF, &truetype.Options{Size: float64(size), DPI: DPI})
+//}
