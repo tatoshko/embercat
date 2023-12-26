d574d0e1d5500c833d649033a3aa03e2d7113bfb
Anton Kim <tatoshko@gmail.com>
2023-12-26 16:14:10 +0300

    auto

--- bot/handlerQuote/drawer/addLabel.go
+++ bot/handlerQuote/drawer/addLabel.go
@@ -1,8 +1,8 @@
 package drawer
 
 import (
-    "github.com/golang/freetype"
     "github.com/golang/freetype/truetype"
+    "golang.org/x/image/font"
     "golang.org/x/image/font/gofont/gobold"
     "golang.org/x/image/math/fixed"
     "image"
@@ -10,48 +10,56 @@ import (
 )
 
 var (
+    text          string
     point         fixed.Point26_6
     srcImg        *image.RGBA
+    fakeImg       *image.RGBA
     imageBounds   fixed.Rectangle26_6
     white                 = color.RGBA{R: 255, G: 255, B: 255, A: 255}
     TTF, _                = truetype.Parse(gobold.TTF)
     StartFontSize         = fixed.Int26_6(42)
     DPI           float64 = 72
+    padding               = 16
 )
 
-func AddLabel(img *image.RGBA, label string) {
+func AddLabel(img *image.RGBA, text string) {
     srcImg = img
     ib := img.Bounds()
     imageBounds = fixed.R(ib.Min.X, ib.Min.Y, ib.Max.X, ib.Max.Y)
-    //drawString(label, StartFontSize)
 
-    pt := freetype.Pt(0, 0)
+    text = text
 
-    ctx := freetype.NewContext()
-    ctx.SetDPI(72)
-    ctx.SetDst(img)
-    ctx.DrawString(label, pt)
+    fakeImg = image.NewRGBA(image.Rect(0, 0, ib.Max.X-padding*2, ib.Max.Y))
+    draw(computeSize(StartFontSize))
 }
 
-//func drawString(label string, size fixed.Int26_6) {
-//    if size == 0 {
-//        return
-//    }
-//
-//    point = fixed.Point26_6{X: 16 * fixed.Int26_6(DPI/4), Y: imageBounds.Max.Y - size*fixed.Int26_6(DPI/4)}
-//    drawer := &font.Drawer{Dst: srcImg, Src: image.NewUniform(white), Face: getFace(size), Dot: point}
-//
-//    sb, _ := drawer.BoundString(label)
-//
-//    if sb.In(imageBounds) {
-//        log.Printf("OK SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
-//        drawer.DrawString(label)
-//    } else {
-//        log.Printf("FAIL SIZE: %d | MIN: %s, MAX: %s | POINT: %s", size, sb.Min, sb.Max, point)
-//        drawString(label, size-1)
-//    }
-//}
-//
-//func getFace(size fixed.Int26_6) font.Face {
-//    return truetype.NewFace(TTF, &truetype.Options{Size: float64(size), DPI: DPI})
-//}
+func computeSize(size fixed.Int26_6) fixed.Int26_6 {
+    if size <= 0 {
+        return 0
+    }
+
+    point = fixed.Point26_6{X: 0, Y: imageBounds.Max.Y - size*fixed.Int26_6(DPI/4)}
+    drawer := &font.Drawer{Dst: fakeImg, Src: image.NewUniform(white), Face: getFace(size), Dot: point}
+
+    sb, _ := drawer.BoundString(text)
+
+    if !sb.In(imageBounds) {
+        return size
+    } else {
+        return computeSize(size - 1)
+    }
+}
+
+func draw(size fixed.Int26_6) {
+    if size == 0 {
+        return
+    }
+
+    point = fixed.Point26_6{X: fixed.Int26_6(padding) * fixed.Int26_6(DPI/4), Y: imageBounds.Max.Y - size*fixed.Int26_6(DPI/4)}
+    drawer := &font.Drawer{Dst: srcImg, Src: image.NewUniform(white), Face: getFace(size), Dot: point}
+    drawer.DrawString(text)
+}
+
+func getFace(size fixed.Int26_6) font.Face {
+    return truetype.NewFace(TTF, &truetype.Options{Size: float64(size), DPI: DPI})
+}
