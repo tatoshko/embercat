4ed87a0a50b05e9a45ccedb31a5a7aa97e4bedd1
Anton Kim <tatoshko@gmail.com>
2024-01-07 10:54:09 +0300

    auto

--- bot/handlerQuote/drawer/Combine.go
+++ bot/handlerQuote/drawer/Combine.go
@@ -30,18 +30,20 @@ func Combine(dst *image.RGBA, src *image.Alpha, position Position) (*image.RGBA,
 
     var combined *image.RGBA
 
+    zeroPoint := image.Pt(0, 0)
+
     switch position {
     case PositionAbove:
         combined = image.NewRGBA(image.Rect(0, 0, dst.Bounds().Max.X, dst.Bounds().Max.Y+src.Bounds().Max.Y))
-        draw.Draw(combined, src.Bounds(), src, image.Point{X: 0, Y: 0}, draw.Src)
+        draw.Draw(combined, src.Bounds(), src, zeroPoint, draw.Src)
         draw.Draw(combined, dst.Bounds(), dst, image.Point{X: 0, Y: src.Bounds().Max.Y}, draw.Src)
     case PositionBelow:
         combined = image.NewRGBA(image.Rect(0, 0, dst.Bounds().Max.X, dst.Bounds().Max.Y+src.Bounds().Max.Y))
-        draw.Draw(combined, dst.Bounds(), dst, image.Point{X: 0, Y: 0}, draw.Src)
-        draw.Draw(combined, src.Bounds(), src, image.Point{X: 0, Y: dst.Bounds().Max.Y}, draw.Src)
+        draw.Draw(combined, dst.Bounds(), dst, zeroPoint, draw.Src)
+        draw.Draw(combined, src.Bounds(), src, image.Pt(0, dst.Bounds().Max.Y), draw.Src)
     case PositionMix:
         combined = dst
-        draw.Draw(combined, src.Bounds(), src, image.Point{X: 0, Y: 0}, draw.Src)
+        draw.Draw(combined, src.Bounds(), src, zeroPoint, draw.Src)
     }
 
     return combined, nil
