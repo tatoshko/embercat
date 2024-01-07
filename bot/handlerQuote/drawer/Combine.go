381e2db4a9b8ff572a8a1300c0c90219c0869f50
Anton Kim <tatoshko@gmail.com>
2024-01-07 11:00:30 +0300

    auto

--- bot/handlerQuote/drawer/Combine.go
+++ bot/handlerQuote/drawer/Combine.go
@@ -4,6 +4,7 @@ import (
     "errors"
     "golang.org/x/image/draw"
     "image"
+    "log"
 )
 
 var (
@@ -34,14 +35,17 @@ func Combine(dst *image.RGBA, src *image.Alpha, position Position) (*image.RGBA,
 
     switch position {
     case PositionAbove:
+        log.Printf("ABOVE")
         combined = image.NewRGBA(image.Rect(0, 0, dst.Bounds().Max.X, dst.Bounds().Max.Y+src.Bounds().Max.Y))
         draw.Draw(combined, src.Bounds(), src, zeroPoint, draw.Src)
         draw.Draw(combined, dst.Bounds(), dst, image.Pt(0, src.Bounds().Max.Y), draw.Src)
     case PositionBelow:
+        log.Printf("Below")
         combined = image.NewRGBA(image.Rect(0, 0, dst.Bounds().Max.X, dst.Bounds().Max.Y+src.Bounds().Max.Y))
         draw.Draw(combined, dst.Bounds(), dst, zeroPoint, draw.Src)
         draw.Draw(combined, src.Bounds(), src, image.Pt(0, dst.Bounds().Max.Y), draw.Src)
     case PositionMix:
+        log.Printf("Mix")
         combined = dst
         draw.Draw(combined, src.Bounds(), src, zeroPoint, draw.Src)
     }
