5e93c986d1c3f2b51bea41e7b55ca63b55ca144e
Anton Kim <tatoshko@gmail.com>
2025-10-16 16:05:58 +0300

    auto

--- bot/bot.go
+++ bot/bot.go
@@ -28,7 +28,7 @@ func Start(config Config) {
             log.Printf("SetHoook error %s\n", err.Error())
         }
 
-        API.Debug = false
+        API.Debug = true
 
         registerCommands()
 
