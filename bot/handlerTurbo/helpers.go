6627fd1c7bab4b69c4b39f58ea2a1d9fa507e03b
Anton Kim <tatoshko@gmail.com>
2025-11-27 10:26:17 +0300

    auto

--- bot/handlerTurbo/helpers.go
+++ bot/handlerTurbo/helpers.go
@@ -7,7 +7,7 @@ import (
 
 func GetRandomLiner() Liner {
     rand.Seed(time.Now().UnixNano())
-    n := rand.Int63n(TOTAL_LINERS-1) + 1
+    n := rand.Int63n(TOTAL_LINERS) + 1
 
     liner, _ := NewLiner(n, 1)
 
